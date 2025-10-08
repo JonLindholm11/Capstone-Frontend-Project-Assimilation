59f0ede4daa565401faceb462fdd2e2ef25471ff
dbistel <ddbistel@gmail.com>
2025-10-07 18:18:27 -0600

    added folder for customer page including two files to hold a get all products and the actual store front. added routing for storefront to the app.jsx. created error 404 page and rout for it in app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import Store from "./Customer/StoreFront";
+import Error404 from "./Error404";
 
 export default function App() {
   return (
@@ -10,6 +12,9 @@ export default function App() {
         <Route index element={<p>Home page</p>} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<Store />} />
+
+        <Route path="*" element={<Error404 />}></Route>
       </Route>
     </Routes>
   );
