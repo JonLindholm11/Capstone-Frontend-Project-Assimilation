688498106ed03cb1f86755a3e08e2daacc6a31f6
dbistel <ddbistel@gmail.com>
2025-10-14 19:54:34 -0600

    added some noted out in getallproducts for when backend is ready

--- src/Customer/components/GetAllProducts.jsx
+++ src/Customer/components/GetAllProducts.jsx
@@ -1,3 +1,5 @@
-//import { useNavigate } from "react-router";
-
-export default function Products() {}
+// export async function Products() {
+//   const response = await fetch("http://localhost:3000/");
+//   if (!response.ok) throw new Error("Failed to fetch products");
+//   return response.json();
+// }
