74fba5c801f15679a5815965a271dfc028857161
dbistel <ddbistel@gmail.com>
2025-10-23 20:29:30 -0600

    updated error 404 page and css

--- src/Error404.jsx
+++ src/Error404.jsx
@@ -1,8 +1,14 @@
+import "./error.css"
 export default function Error404() {
   return (
-    <>
+    <div className="error-page">
       <h1>404</h1>
       <p>Page not found.</p>
-    </>
+      <img
+        className="error-image"
+        src="https://images.pexels.com/photos/34375255/pexels-photo-34375255.jpeg"
+        alt="meme mike wizaski"
+      />
+    </div>
   );
 }
