32eb361cf997fc3752d2c45fd41d06bf4b9cd46d
dbistel <ddbistel@gmail.com>
2025-10-30 18:44:19 -0600

    adjusted css and added an img to hoome page

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -2,14 +2,21 @@ import "./Home.css";
 
 export default function ProductLanding() {
   return (
-    <div>
-      <h1> Noble Market -Where every venture is a noble one</h1>
-      <p>
+    <div className="container">
+      {/* <p>
         Our Mission: Built on the belief that creation is an act of courage, we
         unite food, fabric, steel, and circuit alike under one roof. Here, every
         hand that builds, stitches, or shapes is honored — because every venture
         is a noble one.
-      </p>
+      </p> */}
+      <img
+        src="https://images.pexels.com/photos/375889/pexels-photo-375889.jpeg"
+        alt="market with many items for sale"
+      />
+      <div className="top-left">
+        Noble Market
+        <p>-Where every venture is a noble one</p>
+      </div>
     </div>
   );
 }
