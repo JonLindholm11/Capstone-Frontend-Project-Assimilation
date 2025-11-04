3dcf0f622f4c983a149bd119a8daf83759c1fa26
JodsonCadet <cadetjoddson@gmail.com>
2025-11-04 17:45:09 -0500

    Debugging assigning salesman to customers, still working

--- src/Customer/pages/Cart/Cart.jsx
+++ src/Customer/pages/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { useCart } from "./CartContext";
 import { postApiDataForCustomers } from "./api";
 import "./Cart.css";
